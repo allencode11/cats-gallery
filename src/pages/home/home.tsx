05ccb99399e98760e0e9956ad653d2fe0ba80ff1
Alina Pavalachi <al.el.en.lina@gmail.com>
2023-12-14 09:35:10 +0200

    adding loading component on home page

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { ImageCardComponent } from "../../components/image-card/image-card.tsx";
+import {LoadingComponent} from "../../components/loading/loading.component.tsx";
 
 type ApiData = {
     id: string,
@@ -30,19 +31,24 @@ export default function HomeComponent() {
     };
 
     fetchData();
-    console.log(loading)
-
   }, []);
   return (
     <div className='bg-slate-800 min-h-screen text-slate-50 min-w-screen flex flex-col justify-center items-center'>
-      <div className='text-[160px] max-sm:text-[32px] text-center'>Cats Gallery</div>
-      <div className={'flex flex-wrap justify-center items-center'}>
-        {
-          data.map(element => (
-            <ImageCardComponent key={element.id} imageSrc={element.url} link={element.id} />
-          ))
-        }
-      </div>
+      {
+        loading ? <LoadingComponent /> :
+          (
+            <div>
+              <div className='text-[160px] max-sm:text-[32px] text-center'>Cats Gallery</div>
+              <div className={'flex flex-wrap justify-center items-center'}>
+                {
+                  data.map(element => (
+                    <ImageCardComponent key={element.id} imageSrc={element.url} link={element.id} />
+                  ))
+                }
+              </div>
+            </div>
+          )
+      }
     </div>
   )
 }
